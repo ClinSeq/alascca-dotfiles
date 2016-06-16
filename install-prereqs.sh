f40496b906506b628b44b316cd31a9c2bcde6182
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-06-16 08:40:15 +0200

    Add missing `-y` to conda install

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -8,7 +8,7 @@ conda config --add channels r
 conda config --add channels bioconda
 
 wget -O /tmp/autoseq-conda-list.txt https://raw.githubusercontent.com/dakl/autoseq/master/conda-list.txt
-conda install --file /tmp/autoseq-conda-list.txt
+conda install -y --file /tmp/autoseq-conda-list.txt
 
 conda install -y cryptography psycopg2
 pip install --upgrade git+https://github.com/ClinSeq/referral-manager.git
