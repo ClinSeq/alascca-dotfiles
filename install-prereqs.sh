5915f4a835569faf2d76e0d6fc9acfc9db93ffdb
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-06-17 11:32:43 +0200

    add supervisor

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -12,32 +12,35 @@ conda install -y --file /tmp/autoseq-conda-list.txt
 rm /tmp/autoseq-conda-list.txt
 
 conda install -y cryptography psycopg2
-pip install --upgrade git+https://github.com/ClinSeq/referral-manager.git
 
 pip install --upgrade pydotplus
 pip install --upgrade vcf_parser
-pip install --upgrade git+https://github.com/dakl/localq.git
+pip install --upgrade supervisor
+
+mkdir -p /nfs/ALASCCA/logs
 
 # pip install from github/clinseq
+pip install --upgrade git+https://github.com/ClinSeq/referral-manager.git
+pip install --upgrade git+https://github.com/dakl/localq.git
 pip install --upgrade git+https://github.com/clinseq/multiqc-alascca.git
 pip install --upgrade git+https://github.com/clinseq/pypedream.git
 
 function git_clone_or_pull {
-    if cd $2 ; then 
+    if cd $2 ; then
       git pull
       cd ..
-    else 
-      git clone $1 $2; 
+    else
+      git clone $1 $2;
     fi
 }
 
 git_clone_or_pull https://github.com/dakl/autoseq-scripts /nfs/ALASCCA/autoseq-scripts
-git_clone_or_pull https://bitbucket.org/clinseq/genome-resources /nfs/ALASCCA/genome-resources 
 git_clone_or_pull https://github.com/dakl/autoseq.git /nfs/ALASCCA/autoseq
 pip install --upgrade /nfs/ALASCCA/autoseq
 
 # needs pwd
 git_clone_or_pull https://bitbucket.org/clinseq/aurora.git /nfs/ALASCCA/aurora
+git_clone_or_pull https://bitbucket.org/clinseq/genome-resources /nfs/ALASCCA/genome-resources
 pip install --upgrade /nfs/ALASCCA/aurora
 
 # pip install from bitbucket/clinseq
@@ -51,10 +54,10 @@ fi
 
 ## linuxbrew
 git clone https://github.com/Linuxbrew/linuxbrew.git /nfs/ALASCCA/linuxbrew
-brew install ack tree 
+brew install ack tree
 
 # install R packages
-Rscript install-r-packages.R                                                                                                              
+Rscript install-r-packages.R
 
 ## TexLive 2015
 cd /tmp
@@ -63,7 +66,7 @@ tar -zxvf install-tl-unx.tar.gz
 cd install-tl-*
 ./install-tl -profile /nfs/ALASCCA/alascca-dotfiles/texlive.profile
 
-echo 
+echo
 echo "you should now run "
 echo generate-ref --genome-resources /nfs/ALASCCA/genome-resources --outdir /nfs/ALASCCA/autoseq-genome
 echo "to generate reference files required by autoseq"
